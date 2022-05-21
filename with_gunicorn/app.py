991fd99b1d6dc62078d75042542156b2bce9475d
Goto Hayato <habita.gh@gmail.com>
2022-05-21 16:55:58 +0900

    Return text `404 Not Found` when no file matched in Gunicorn sample.

--- with_gunicorn/app.py
+++ with_gunicorn/app.py
@@ -2,23 +2,37 @@
 
 from mimetypes import guess_type
 from pathlib import Path
+from typing import Optional
 
-ROOT_DIR = (Path(__file__).resolve().parent.parent / 'htdocs')
+ROOT_DIR = Path(__file__).resolve().parent.parent / 'htdocs'
 
 
 def application(environ, start_response):
     path = ROOT_DIR / environ['PATH_INFO'].lstrip('/')
 
-    data = b''
-    if path.is_relative_to(ROOT_DIR) and path.is_file():
-        data = path.read_bytes()
+    file = locate_target_file(ROOT_DIR, path)
+    if file is None:
+        start_response('404 Not Found', [('Content-Length', '0')])
+        return iter([b''])
 
     status = '200 OK'
-    (mimetype, encoding) = guess_type(path)
+    data = file.read_bytes()
+    (mimetype, encoding) = guess_type(file)
     response_headers = [
-        ('Content-type', mimetype or 'text/plain'),
-        ('Content-Length', str(len(data)))
+        ('Content-type', mimetype),
+        ('Content-Length', str(len(data))),
     ]
-
     start_response(status, response_headers)
     return iter([data])
+
+
+def locate_target_file(root: Path, path: Path) -> Optional[Path]:
+    if path.is_relative_to(root):
+        if path.is_file():
+            return path
+        if path.is_dir():
+            index_file = path / 'index.html'
+            if index_file.is_file():
+                return index_file
+
+    return None
