669bc1bbf9140ea02a1a30b7ef183a0468506082
Goto Hayato <gh640@hey.com>
2023-05-14 22:00:15 +0900

    refactor: 💡 `static_folder` can be a Path-like object

--- with_gunicorn_flask/app.py
+++ with_gunicorn_flask/app.py
@@ -4,6 +4,6 @@ from pathlib import Path
 
 from flask import Flask
 
-STATIC_FOLDER = str(Path(__file__).resolve().parent.parent / 'htdocs')
+STATIC_FOLDER = Path(__file__).resolve().parents[1] / 'htdocs'
 
 application = Flask(__name__, static_url_path='', static_folder=STATIC_FOLDER)
